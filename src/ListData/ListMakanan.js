d982e459877db215e35f37e9eb11865d192a7746
wiladhianty <wiladhianty@gmail.com>
2021-07-01 14:57:56 +0700

    tambah tugas

---
+++ src/ListData/ListMakanan.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react"
+
+class ListMakanan extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            datalist: this.props.link
+        }
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <img src = {this.state.datalist} alt="Product Makanan" width="150px"></img>
+            </div>
+        )
+    }
+}
+
+
+export default ListMakanan
---
+++ src/Page/MenuKontak.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react"
+
+class MenuKontak extends Component{
+    constructor(props){
+        super(props);
+        this.ket ={
+            alamat : "Jl. Swadaya IV, Pd. Ranggon, Cipayung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13860",
+            kontak : "081312345678"
+        }
+
+
+
+    }
+    render(){
+        return(
+            <div>
+                <center>
+                    <h3>{this.ket.alamat}</h3>
+                    <h4>Kontak Kami : {this.ket.kontak}</h4>
+                </center>
+            </div>
+        )
+    }
+}
+
+
+export default MenuKontak
